f25514bee55e54768076ee75cc1dc232d414d31d
HCG\Patrik.Mihalcin <patrik.mihalcin@embedit.cz>
2018-10-09 13:23:25 +0200

    Add @Id

--- src/main/java/mihalcin/specification/Specification.java
+++ src/main/java/mihalcin/specification/Specification.java
@@ -1,12 +1,16 @@
 package mihalcin.specification;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 import lombok.RequiredArgsConstructor;
 
 @Entity
 @RequiredArgsConstructor
 public class Specification {
+
+    @Id
+    private Long id;
 
     private final String name;
 
